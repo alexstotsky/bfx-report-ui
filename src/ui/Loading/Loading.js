6722325e524b57c6be5c7ef1e6bf28e7bba7b14b
alexstotsky <stotsky.alex@gmail.com>
2024-05-20 12:53:32 +0300

    Provide optional progress support for loader

--- src/ui/Loading/Loading.js
+++ src/ui/Loading/Loading.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Spinner } from '@blueprintjs/core'
 
-export const Loading = () => (
+export const Loading = (showProgress) => (
   <div className='loading-container'>
+    {showProgress && (
+    <div className='loading-container'>
+      100%
+    </div>
+    )}
     <Spinner
       className='loading'
       size={Spinner.SIZE_STANDARD}
     />
   </div>
 )
+
+Loading.propTypes = {
+  showProgress: PropTypes.bool,
+}
+
+Loading.defaultProps = {
+  showProgress: false,
+}
 
 export default Loading
