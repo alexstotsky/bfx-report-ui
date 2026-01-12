bf46910784433cc6a667bead8a26e0f70e4001d2
alexstotsky <stotsky.alex@gmail.com>
2026-01-12 14:06:44 +0200

    Remove turkish site related logic from the nav menu helpers

--- src/ui/NavMenu/NavMenu.helpers.js
+++ src/ui/NavMenu/NavMenu.helpers.js
@@ -56,7 +56,7 @@ const getSubSectionsTitle = (isFrameworkMode) => (
     : 'weightedaverages.title'
 )
 
-export const getSections = (menuType, isTurkishSite) => {
+export const getSections = (menuType) => {
   switch (menuType) {
     case MENU_MY_ACCOUNT:
       return [
@@ -71,18 +71,18 @@ export const getSections = (menuType, isTurkishSite) => {
         [MENU_LEDGERS, 'ledgers.title'],
         [MENU_MOVEMENTS, 'movements.title'],
         [MENU_WALLETS, 'wallets.title'],
-        [MENU_FPAYMENT, 'navItems.myHistory.earnings', isTurkishSite, EARNINGS_TARGETS],
+        [MENU_FPAYMENT, 'navItems.myHistory.earnings', EARNINGS_TARGETS],
         [[MENU_TRADES, MENU_CANDLES], 'trades.title'],
         [[MENU_ORDERS, MENU_ORDER_TRADES], 'orders.title'],
         [[MENU_POSITIONS, MENU_POSITIONS_ACTIVE, MENU_POSITIONS_AUDIT], 'positions.title'],
-        [MENU_FOFFER, 'navItems.myHistory.funding', isTurkishSite, FUNDING_TARGETS],
+        [MENU_FOFFER, 'navItems.myHistory.funding', FUNDING_TARGETS],
       ]
     case MENU_MARKET_HISTORY:
       return [
         [MENU_PUBLIC_TRADES, 'navItems.marketHistory.trades'],
-        [MENU_PUBLIC_FUNDING, 'navItems.marketHistory.funding', isTurkishSite],
+        [MENU_PUBLIC_FUNDING, 'navItems.marketHistory.funding'],
         [MENU_TICKERS, 'navItems.marketHistory.spot'],
-        [MENU_DERIVATIVES, 'navItems.marketHistory.derivatives', isTurkishSite],
+        [MENU_DERIVATIVES, 'navItems.marketHistory.derivatives'],
       ]
     default:
       return []
